6d564c2d1719b3385db8e930cc76f47a68dfa99b
yousra <yousra@yousras-mbp.home>
2025-10-05 00:44:45 +0200

    add

--- cypress.config.js
+++ cypress.config.js
@@ -1,7 +1,7 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  backendUrl: "http://localhost:5000",
+  backendUrl: "https://bookcafe2.onrender.com",
   e2e: {
     // We've imported your old cypress plugins here.
     // You may want to clean this up later by importing these.
